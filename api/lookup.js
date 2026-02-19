4b4869d852d2d9be6d437476513a1865af90acf3
yassclean <yassclean@users.noreply.github.com>
2026-02-19 10:52:52 -0500

    Fix: use CommonJS module.exports for Vercel serverless function

--- api/lookup.js
+++ api/lookup.js
@@ -1,7 +1,7 @@
 // Vercel serverless function for property lookup
 // Merges Rentcast (sqft) + Perplexity (beds/baths)
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
